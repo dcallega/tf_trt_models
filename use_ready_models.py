0d39a5de9bfeeb8f6e497c006c6943a07dfa48eb
dcallega <dcallega@uci.edu>
2019-06-19 18:40:27 -0700

    Adding save tegrastats

--- use_ready_models.py
+++ use_ready_models.py
@@ -10,14 +10,14 @@ import matplotlib.patches as patches
 import tensorflow as tf
 import numpy as np
 import time
-from tf_trt_models.detection import download_detection_model, build_detection_graph
+from tf_trt_models.detection import *
 
-# MODELS = ['ssdlite_mobilenet_v2_coco']
-# MODEL = MODELS[0]
-# DATA_DIR = './data/'
-# CONFIG_FILE = MODEL + '.config'   # ./data/ssd_inception_v2_coco.config 
-# CHECKPOINT_FILE = 'model.ckpt'    # ./data/ssd_inception_v2_coco/model.ckpt
-# IMAGE_PATH = './data/huskies.jpg'
+MODELS = ['ssdlite_mobilenet_v2_coco']
+MODEL = MODELS[0]
+DATA_DIR = './data/'
+CONFIG_FILE = MODEL + '.config'   # ./data/ssd_inception_v2_coco.config 
+CHECKPOINT_FILE = 'model.ckpt'    # ./data/ssd_inception_v2_coco/model.ckpt
+IMAGE_PATH = './data/huskies.jpg'
 
 # config_path, checkpoint_path = download_detection_model(MODEL, 'data')
 # frozen_graph, input_names, output_names = build_detection_graph(
@@ -44,10 +44,13 @@ from tf_trt_models.detection import download_detection_model, build_detection_gr
 
 def get_frozen_graph(graph_file):
     """Read Frozen Graph file from disk."""
-    with tf.gfile.FastGFile(graph_file, "rb") as f:
+    with tf.gfile.GFile(graph_file, "rb") as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
     return graph_def
+
+input_names = [INPUT_NAME]
+output_names = [BOXES_NAME, CLASSES_NAME, SCORES_NAME, NUM_DETECTIONS_NAME]
 
 trt_graph = get_frozen_graph("data/ssd_inception_v2_coco_trt.pb")
 tf_config = tf.ConfigProto()
@@ -57,6 +60,8 @@ tf_sess = tf.Session(config=tf_config)
 
 tf.import_graph_def(trt_graph, name='')
 
+print("Session ready")
+
 tf_input = tf_sess.graph.get_tensor_by_name(input_names[0] + ':0')
 tf_scores = tf_sess.graph.get_tensor_by_name('detection_scores:0')
 tf_boxes = tf_sess.graph.get_tensor_by_name('detection_boxes:0')
@@ -65,7 +70,7 @@ tf_num_detections = tf_sess.graph.get_tensor_by_name('num_detections:0')
 
 image = Image.open(IMAGE_PATH)
 
-plt.imshow(image)
+# plt.imshow(image)
 
 image_resized = np.array(image.resize((300, 300)))
 image = np.array(image)
@@ -87,15 +92,15 @@ ax.imshow(image)
 
 # plot boxes exceeding score threshold
 for i in range(int(num_detections)):
-    # scale box to image coordinates
-    box = boxes[i] * np.array([image.shape[0], image.shape[1], image.shape[0], image.shape[1]])
+  # scale box to image coordinates  
+  box = boxes[i] * np.array([image.shape[0], image.shape[1], image.shape[0], image.shape[1]])
 
-    # display rectangle
-    patch = patches.Rectangle((box[1], box[0]), box[3] - box[1], box[2] - box[0], color='g', alpha=0.3)
-    ax.add_patch(patch)
+  # display rectangle
+  patch = patches.Rectangle((box[1], box[0]), box[3] - box[1], box[2] - box[0], color='g', alpha=0.3)
+  ax.add_patch(patch)
 
-    # display class index and score
-    plt.text(x=box[1] + 10, y=box[2] - 10, s='%d (%0.2f) ' % (classes[i], scores[i]), color='w')
+  # display class index and score
+  plt.text(x=box[1] + 10, y=box[2] - 10, s='%d (%0.2f) ' % (classes[i], scores[i]), color='w')
 
 plt.show()
 
