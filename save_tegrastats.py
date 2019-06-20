777b2790d04ee13389eb9ed3758f38fb56872194
Davide <dcallega@uci.edu>
2019-06-20 09:23:07 -0700

    Making it work on Nvidia Jetson Nano

--- save_tegrastats.py
+++ save_tegrastats.py
@@ -3,6 +3,7 @@ import signal
 import subprocess
 import time
 from statistics import mean
+import time
 
 LOG_FILE = None
 
@@ -17,37 +18,38 @@ def exit_pro(signum, frame):
 signal.signal(signal.SIGINT, exit_pro)
 signal.signal(signal.SIGTERM, exit_pro)
 # where the tegrastats binary file is
-BIN_PATH = '../../tegrastats'
-LOG_FILE_PATH = './freq.log'
+BIN_PATH = 'tegrastats'
+LOG_FILE_PATH = 'LOGS.csv'
 
 
 def work(write_to_log=False, your_args=''):
     """@:arg write_to_log """
     global LOG_FILE
-    cmds = [BIN_PATH]
+    cmds = [BIN_PATH, "--interval",  "100"]
     if your_args is None:
       your_args = ''
     # cmds += your_args.split()
     p = subprocess.Popen(cmds, stdout=subprocess.PIPE)
     if write_to_log:
-        LOG_FILE = open(LOG_FILE_PATH, 'a')
+        LOG_FILE = open(LOG_FILE_PATH, 'a+')
     while True:
         current_stat = p.stdout.readline().decode().strip()
         if current_stat == '':
             print("tegrastats error")
             break
         fields = current_stat.split(" ")
-        stats = {}
+        stats, others = {}, {}
         stats["ram_used"], stats["ram_tot"] = [int(e) for e in fields[1][:-2].split("/")]
         others["cpu_perc_freq"] = [[int(e.split("%@")[0]), int(e.split("%@")[1])] for e in fields[5][1:-1].split(",")]
         stats["avg_used"] = mean([perc/100. for perc, freq in others["cpu_perc_freq"]])
-        stats["avg_freq"] = mean([freq for perc, freq in stats["cpu_perc_freq"]])
+        stats["avg_freq"] = mean([freq for perc, freq in others["cpu_perc_freq"]])
         # Weighted average frequency
-        stats["w_avg_freq"] = mean([perc/100. *freq for perc, freq in stats["cpu_perc_freq"]]) 
+        stats["w_avg_freq"] = mean([perc/100. *freq for perc, freq in others["cpu_perc_freq"]]) 
         stats["emc"] = int(fields[7][:-1]) # ext_mem_contr_freq_perc_used
         stats["gpu_used"] = int(fields[9][:-1])
-        [stats["pom_5v_{}".format(fields[i].split("_")[-1])] = int(fields[i+1].split("/")[0]) for i in [16, 18, 20]]
-        text = ",".join(stats[e] for e in sorted(stats))
+        for i in [16, 18, 20]:
+          stats["pom_5v_{}".format(fields[i].split("_")[-1])] = int(fields[i+1].split("/")[0])
+        text = str(time.time()) + "," + ",".join([str(stats[e]) for e in sorted(stats)])
         if write_to_log:
             LOG_FILE.write(text + '\n')
 
